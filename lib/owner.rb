7e8b440e70ebfc24864dc7c778b25b22388c37ed
Paula McKinley <pmknyc@optimum.net>
2018-12-04 20:31:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,17 @@
 class Owner
-  # code goes here
+
+  attr_accessor  :pets, :fishes, :cats, :dogs
+  @@all = []
+  
+  def initialize
+    @pets = Hash.new{:fishes => [], :cats => [], :dogs => []}
+  end
+
+  def self.all
+    @@all << self
+  end
+
+
+
+#class end below
 end
