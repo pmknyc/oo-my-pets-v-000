7c274a66185e45bc4f5b132c1edeed8159ed8cb9
Paula McKinley <pmknyc@optimum.net>
2018-12-05 01:27:34 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor mood:
+  attr_reader name:
+
+  def initialize(name, mood = "nervous")
+    @mood #initialize w/ default nervous mood
+    @name = name
+  end
+
+
+
+#class end below
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,38 @@
 class Owner
-  # code goes here
+
+  attr_accessor :pets, :fishes, :cats, :dogs, :count
+  attr_reader :species
+  @@all = []
+  @@count = 0
+
+  def initialize
+    @pets = Hash.new{:fishes => [], :cats => [], :dogs => []}
+    @species = "human"
+    self.count
+    self.all
+  end
+
+# Class methods
+  def self.all
+    @@all << self
+  end
+
+  def self.count #iadds to Owner count; called by initialize method
+    @@count +=1
+  end
+
+  def self.reset_all
+    @@count = 0
+    @@all = []
+  end
+
+# Instance methods
+  def say_species
+    puts "I am a #{species}."
+  end
+
+  def name
+
+  end
+#class end below
 end
